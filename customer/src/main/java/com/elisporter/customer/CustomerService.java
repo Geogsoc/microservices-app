98a2766bd81b2551d66159751c359df022dd18be
Elis Porter <elisporter80@gmail.com>
2025-02-22 10:29:08 -0800

    add eureka server

--- customer/src/main/java/com/elisporter/customer/CustomerService.java
+++ customer/src/main/java/com/elisporter/customer/CustomerService.java
@@ -1,24 +1,36 @@
 package com.elisporter.customer;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service //can also be @Component
 @AllArgsConstructor
+@Slf4j
 public class CustomerService {
 
+    private final RestTemplate restTemplate;
+
     private final CustomerRepository customerRepository;
+
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
 
-Customer customer = Customer.builder()
-        .firstName(customerRegistrationRequest.firstName())
-        .lastName(customerRegistrationRequest.lastName())
-        .email(customerRegistrationRequest.email())
-        .build();
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
 
+        customerRepository.saveAndFlush(customer);
 
-customerRepository.save(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
 
+        if (fraudCheckResponse.isFrauster()) {
+            log.info("customer is fraudster: {}",customer.getId());
+            throw new IllegalStateException("Is fraudster");
+        }
+        log.info("customer is not a fraudster: {}", customer.getId());
 //todo: validation
 
     }
