310812c3abaf817e6c90c1396e56b0617db25233
Elis Porter <elisporter80@gmail.com>
2025-03-21 15:11:57 -0700

    containerise apps

--- notification/src/main/java/com/elisporter/notification/NotificationApplication.java
+++ notification/src/main/java/com/elisporter/notification/NotificationApplication.java
@@ -15,11 +15,9 @@ import org.springframework.context.annotation.PropertySources;
                 "com.elisporter.amqp",
         }
 )
-//@PropertySources({
-//        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
-//})
 
-@EnableEurekaClient
+@PropertySource("classpath:clients-${spring.profiles.active}.properties")
+//@EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
